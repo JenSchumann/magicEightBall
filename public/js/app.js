4c1e398b29e59067bf2d4ad435d7925db0b7b09b
Jen Schumann <jenniferschumann1@gmail.com>
2018-03-28 19:11:02 -0600

    added the prophetic functionality

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,27 @@
 console.log('ready to prophesy');
+// var output = document.getElementById("output");
+
+function askQ(){
+    var answer = "something went wrong";
+       var output = document.getElementById("output");
+
+       var question = prompt("Ask me anything");
+       var randomNumber = Math.floor(Math.random()*6);
+       switch (randomNumber){
+           case 0:
+               answer = "It will work out.";
+               break;
+           case 1:
+               answer = "There's a slight possibility.";
+               break;
+           case 2:
+               answer = "Probably not.";
+               break;
+           case 3:
+               answer = "Highly likely.";
+           default:
+               answer = "I don't know about that.";
+       }
+       output.innerHTML = "You asked me "+question+"<br>Here's my answer: "+answer
+     }
+  // output.innerHTML = "You asked me "+question+"<br>Here's my answer: "+answer
